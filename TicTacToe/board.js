d0f9d9bf2a85e169a64096225bba5b3873188593
shwetambry <shweta@electron.local>
2019-05-06 16:09:12 +1000

    kids math page updated

--- TicTacToe/board.js
+++ TicTacToe/board.js
@@ -3,28 +3,26 @@ var winComb = [['00','01','02'],['10','11','12'],['20','21','22'],
 ['00','10','20'],['01','11','21'],['02','12','22'],
 ['00','11','22'],['02','11','20']];
 var numPlayerRadio = 1;
-var pl1symb = 'X';
-var pl2symb = 'O';
+var curplsymb = 'X';
+//var pl2symb = 'O';
 var game_over = false;
 function getNumPl(){
 	numPlayerRadio = $("input[name='players']:checked").val();
-	//return numPlayerRadio;
+	
 }
 function getplSymb(){
-	pl1symb = $("input[name='optradio']:checked").val();
-	pl2symb = $("input[name='optradio']:not(:checked)").val();
-	//return pl1symb;
+	curplsymb = $("input[name='optradio']:checked").val();
+	//pl2symb = $("input[name='optradio']:not(:checked)").val();
+	
 }
-function checkWin(rad){
+function checkWin(sym){
 	for(var i = 0; i<8;i++){
 		var comb = winComb[i];
-		if ($('#'+comb[0]).text() == rad){
-			if($('#'+comb[1]).text() == rad && $('#'+comb[2]).text() == rad){
-				$('#msg').html("Game Over!!! "+ rad + " wins.");
+		if ($('#'+comb[0]).text() == sym){
+			if($('#'+comb[1]).text() == sym && $('#'+comb[2]).text() == sym){
+				$('#msg').html("Game Over!!! "+ sym + " wins.");
 				$('#msg').css('color','blue');
-				
 				endGame();
-				
 				break;
 			} 
 		}
@@ -33,6 +31,14 @@ function checkWin(rad){
 function endGame(){
 	game_over = true;
 	$("button.sq").prop("disabled", true);
+}
+function toggleTurns(){
+	if(curplsymb == 'X'){
+		curplsymb = 'O';
+	}
+	else{
+		curplsymb = 'X';
+	}
 }
 $(document).ready(function(){
 	$("input[name='players']").change(function(){
@@ -46,6 +52,7 @@ $(document).ready(function(){
 	$('#reset').click(function(){
 		document.location.reload(true);
 	});
+
 	$('button.sq').click(function(){
 		$("input[type=radio]").attr('disabled',true);
 		var  okay = false;
@@ -62,9 +69,10 @@ $(document).ready(function(){
 		}
 		else{
 			$('#'+curID).css('background-color','maroon');
-			$('#'+curID).html(pl1symb).promise().done(checkWin(pl1symb));
+			$('#'+curID).html(curplsymb).promise().done(checkWin(curplsymb));
 			if(game_over == false){
 				if (freeSq.length > 0){
+					toggleTurns();
 					if(numPlayerRadio == 1){
 						setTimeout(function(){
 							var randInd = Math.floor(Math.random()*freeSq.length);
