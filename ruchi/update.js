bd31462e137b6770c46e4e286b4852e024de1bc0
Shweta Tejpal <stejpal@deloitte.com.au>
2020-03-02 23:45:26 +1100

    Made corrections and slideshow

--- ruchi/update.js
+++ ruchi/update.js
@@ -1,10 +1,53 @@
+var slideshowFlag = true;
 $(document).ready(function () {
-	$("#myModal").modal({ show: true, backdrop: "static" });
+	"use strict";
+
+	//$("#myModal").modal({ show: true, backdrop: "static" });
 
 	// Hide the Modal
-	$("#agree").click(function () {
-		$("#myModal").modal("hide");
-	});
+	//$("#agree").click(function () {
+	//	$("#myModal").modal("hide");
+	//	slideshowFlag = true;
+	//});
+
+	var slideshow = (function () {
+		if (slideshowFlag) {
+			var counter = 0,
+				i,
+				j,
+				slides = $("#homepage-slideshow .slide"),
+				slideslen = slides.length - 1;
+			for (i = 0, j = 99999; i < slides.length; i += 1, j -= 1) {
+				$(slides[i]).css("z-index", j);
+			}
+			return {
+				startSlideshow: function () {
+					window.setInterval(function () {
+
+						if (counter === 0) {
+							slides.eq(counter).fadeOut();
+							counter += 1;
+
+						}
+						else if (counter === slideslen) {
+							counter = 0;
+							slides.eq(counter).fadeIn(function () {
+								slides.fadeIn();
+
+							});
+						}
+						else {
+							slides.eq(counter).fadeOut();
+							counter += 1;
+
+						}
+					}, 2000);
+				}
+			};
+
+		}
+	}());
+	slideshow.startSlideshow();
 
 	$("a.navItem").click(function () {
 		$('a.navItem.active').removeClass('active');
@@ -17,6 +60,12 @@ $(document).ready(function () {
 		$('#' + contentID).removeClass('inactive');
 		$('#' + contentID).addClass('active');
 
+	});
+
+	$(".dot").click(function () {
+		var curID = $(this).attr('id');
+
+		showSlides(Number(curID[3]));
 	});
 
 	$(".panel-default").click(function () {
@@ -39,4 +88,55 @@ $(document).ready(function () {
 
 
 
-}); 
+
+
+});
+
+function showSlides(n) {
+	$('.dot.active').removeClass('active');
+	$('.mySlides.active').removeClass('active');
+
+	var slides = $('.slideshow-container').find('.mySlides');
+	var indicators = $('.slideshow-container').find('.dot');
+
+
+
+	$(slides[n]).addClass('active');
+	$(indicators[n]).addClass('active');
+
+}
+var slideshow1 = (function () {
+	var counter = 0,
+		i,
+		j,
+		slides = $("#homepage-slideshow .slide"),
+		slideslen = slides.length - 1;
+	for (i = 0, j = 99999; i < slides.length; i += 1, j -= 1) {
+		$(slides[i]).css("z-index", j);
+	}
+	return {
+		startSlideshow1: function () {
+			window.setInterval(function () {
+				console.log("This is startslideshow ")
+				if (counter === 0) {
+					slides.eq(counter).fadeOut(10000);
+					counter += 1;
+					alert("first if " + counter);
+				}
+				else if (counter === slideslen) {
+					counter = 0;
+					slides.eq(counter).fadeIn(function () {
+						slides.fadeIn(10000);
+						alert("second if " + counter);
+					});
+				}
+				else {
+					slides.eq(counter).fadeOut(10000);
+					counter += 1;
+					alert("last if " + counter);
+				}
+			}, 100000);
+		}
+	};
+}());
+
