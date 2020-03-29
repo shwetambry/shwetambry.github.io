4474bfab4a46f090c405b1131627ebccfbab7b6b
Shweta Tejpal <stejpal@deloitte.com.au>
2020-03-29 20:08:14 +1100

    Menu bar updated

--- ruchi/update.js
+++ ruchi/update.js
@@ -2,6 +2,12 @@ var cur_page_id = "";
 var cur_prime_id = "";
 
 $(document).ready(function() {
+  document.querySelector("#nav-toggle").addEventListener("click", function() {
+    this.classList.toggle("active");
+  });
+  $("#nav-toggle").click(function() {
+    $("nav ul").toggle();
+  });
   //$("#myModal").modal({ show: true, backdrop: "static" });
 
   // Hide the Modal
@@ -9,20 +15,16 @@ $(document).ready(function() {
   //	$("#myModal").modal("hide");
   //	slideshowFlag = true;
   //});
+  /*
   $(".nav-dropdown").css({
     width: $("nav").width() + "px"
   });
   var cumWidths = mapWidth("prime");
-  $("#" + cur_page_id + "_slideshow > div:gt(0)").hide();
-  setInterval(function() {
-    $("#" + cur_page_id + "_slideshow > div:first")
-      .fadeOut()
-      .next()
-      .fadeIn()
-      .end()
-      .appendTo("#" + cur_page_id + "_slideshow");
-  }, 5000);
+  
+  */
+  // slideshow code
 
+  /*
   $("a.navItem").click(function(e) {
     if ($(this).hasClass("prime")) {
       $("a.navItem.active").removeClass("active");
@@ -39,10 +41,11 @@ $(document).ready(function() {
       var ind = $(this)
         .parent()
         .index();
-
+      /*
       $(".nav-dropdown").css({
         left: -cumWidths[ind] - ind * 40 + "px"
       });
+      
     } else if (cur_page_id != cur_prime_id) {
       $("#" + cur_page_id).removeClass("active");
     }
@@ -56,7 +59,47 @@ $(document).ready(function() {
     $("#" + contentID).removeClass("inactive");
     $("#" + contentID).addClass("active");
   });
+  // making sure that when the page opens, submenu of home is open.
   $("#home").trigger("click");
+
+  */
+  // start of the slideshow
+  showSlideshow("home");
+  $("nav ul li a.prime").click(function(e) {
+    cur_prime_id = $(this).attr("id");
+    $("a.prime.active").removeClass("active");
+    $(this).addClass("active");
+    cur_page_id = "";
+    toggleSection(cur_prime_id);
+    $(this)
+      .siblings(".nav-dropdown")
+      .toggle();
+    $(".nav-dropdown")
+      .not($(this).siblings())
+      .hide();
+    e.stopPropagation();
+  });
+  $("html").click(function() {
+    $(".nav-dropdown").hide();
+  });
+
+  $("a.subnav").click(function() {
+    cur_page_id = $(this).attr("id");
+    toggleSection(cur_page_id);
+  });
+  /*
+  // hover over navitem functionality
+  $("nav ul li a").hover(function(e) {
+    $(this)
+      .siblings(".nav-dropdown")
+      .toggle();
+    $(".nav-dropdown")
+      .not($(this).siblings())
+      .hide();
+    e.stopPropagation();
+  });
+  */
+  // accordion behaviour for FAQs
   $(".panel-default").click(function() {
     var panelTitle = $(this).find(".panel-heading");
     var selected = $(this).find(".collapse");
@@ -75,12 +118,40 @@ $(document).ready(function() {
   });
   */
 });
-function getLeft(id) {
-  var index = $("#" + id)
-    .parent()
-    .index();
-  return index;
+
+function toggleSection(cur_id) {
+  var contentID = `${cur_id}_section`;
+  var oldSectionID = $(".section.active").attr("id");
+  $("#" + oldSectionID).removeClass("active");
+  $("#" + oldSectionID).addClass("inactive");
+  $("#" + contentID).removeClass("inactive");
+  $("#" + contentID).addClass("active");
+  addBreadcrumb();
+  showSlideshow(cur_id);
 }
+
+function addBreadcrumb() {
+  var mainNav = $("#" + cur_prime_id).html();
+  var subNav = "";
+  if (cur_page_id != "") {
+    subNav = $("#" + cur_page_id).html();
+  }
+
+  $("#breadcrumb").html(mainNav + " > " + subNav);
+}
+
+function showSlideshow(cur_id) {
+  $("#" + cur_id + "_slideshow > div:gt(0)").hide();
+  setInterval(function() {
+    $("#" + cur_id + "_slideshow > div:first")
+      .fadeOut()
+      .next()
+      .fadeIn()
+      .end()
+      .appendTo("#" + cur_id + "_slideshow");
+  }, 5000);
+}
+/*
 function mapWidth(class1) {
   var widthArray = [0];
   var cumsum = 0;
@@ -92,3 +163,4 @@ function mapWidth(class1) {
 
   return widthArray;
 }
+*/
